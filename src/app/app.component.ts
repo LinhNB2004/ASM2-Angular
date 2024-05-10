317a7c223d699183aeb51aac4af914a4bc3c48b3
LinhNB2004 <linhlinh26052004@gmail.com>
2024-05-10 12:12:49 +0700

    buoi2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { ProductListComponent } from './product-list/product-list.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListComponent],
+  imports: [
+    RouterOutlet,
+    ProductListComponent,
+    HomePageComponent,
+    NotFoundComponent,
+    FormsModule,
+  ],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'WD18332';
-  userInfor = {
+  user = {
     name: 'LÊ THỊ THÙY LINH',
     age: 20,
     address: 'Ninh Bình',
@@ -20,5 +29,8 @@ export class AppComponent {
   };
   hello() {
     console.log('Xin chào!');
+  }
+  showInfor() {
+    console.log('Hien thi thong tin ca nhan!');
   }
 }
